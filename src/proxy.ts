60c747bba9c99178275b6cc2f8f85b5d01c34bc1
MukitHossen7 <hossenmukit7@gmail.com>
2025-12-02 21:51:36 +0600

    feat: implement proxy middleware for authentication and role-based access control

---
+++ src/proxy.ts
@@ -0,0 +1,82 @@
+import { NextRequest, NextResponse } from "next/server";
+import { deleteCookies, getCookies } from "./services/auth/tokenHandler";
+import {
+  getDefaultDashboardRoutes,
+  getRouteOwner,
+  isAuthRoutes,
+  UserRole,
+} from "./utility/auth-utils";
+import { JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+
+export async function proxy(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const accessToken = await getCookies("accessToken");
+
+  let userRole: UserRole | null = null;
+
+  if (accessToken) {
+    const verifyToken: JwtPayload | string = jwt.verify(
+      accessToken,
+      process.env.ACCESS_TOKEN_SECRET as string
+    );
+
+    if (typeof verifyToken === "string") {
+      await deleteCookies("accessToken");
+      await deleteCookies("refreshToken");
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+    userRole = verifyToken.role as UserRole;
+  }
+  const routeOwner = getRouteOwner(pathname);
+  const isAuth = isAuthRoutes(pathname);
+
+  // Role: 1
+  if (accessToken && isAuth) {
+    return NextResponse.redirect(
+      new URL(getDefaultDashboardRoutes(userRole as UserRole), request.url)
+    );
+  }
+
+  // Role: 2
+  if (routeOwner === null) {
+    return NextResponse.next();
+  }
+
+  if (!accessToken) {
+    const loginUrl = new URL("/login", request.url);
+    loginUrl.searchParams.set("redirect", pathname);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  //Rule : 3
+  if (routeOwner === "COMMON") {
+    return NextResponse.next();
+  }
+
+  // Rule : 4
+
+  if (routeOwner === "ADMIN" || routeOwner === "TRAVELER") {
+    if (routeOwner === userRole) {
+      return NextResponse.redirect(
+        new URL(getDefaultDashboardRoutes(userRole as UserRole), request.url)
+      );
+    }
+    return NextResponse.next();
+  }
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
